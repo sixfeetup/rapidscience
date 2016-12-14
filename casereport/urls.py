3ff91709f6f482bb608f434fa360feb12a410c25
Rob McBroom <rob@sixfeetup.com>
2016-12-14 13:38:47 -0500

    PEP8 and readability improvements

--- casereport/urls.py
+++ casereport/urls.py
@@ -1,24 +1,34 @@
-from django.views.generic import TemplateView
-from .views import CaseReportFormView
-from  .views import AutoCompleteView
-
-__author__ = 'yaseen'
 from django.conf.urls import patterns, url
+from django.views.generic import TemplateView
 
+from .views import AutoCompleteView
 from .views import CaseReportDetailView
+from .views import CaseReportEditView
+from .views import CaseReportFormView
 from .views import downloadfile
 from .views import FormTypeView
 from .views import reindexsolr
-from  .views import CaseReportEditView
 
-urlpatterns = patterns('',
-                       url(r'^results/$', TemplateView.as_view(template_name="search_results.html")),
-                       url(r'^add/$', CaseReportFormView.as_view(), name='add_casereport'),
-                       url(r'^autocomplete/$', AutoCompleteView.as_view(), name='autocomplete'),
-                       url(r'^formtype/$', FormTypeView.as_view(), name='ftype'),
-                       url(r'^reindex/$', reindexsolr),
-                       url(r'^(?P<case_id>[0-9]*)/(?P<title_slug>.*)/$',
-                           CaseReportDetailView.as_view(), name='casereport_detail'),
-                       url(r'^download/(?P<file_id>.*)/$', downloadfile, name='download'),
-                       url(r'^edit/(?P<case_id>[0-9]*)/(?P<token>.*)/$', CaseReportEditView.as_view()),
-                       )
+
+__author__ = 'yaseen'
+
+urlpatterns = patterns(
+    '',
+    url(
+        r'^results/$',
+        TemplateView.as_view(template_name="search_results.html"),
+    ),
+    url(r'^add/$', CaseReportFormView.as_view(), name='add_casereport'),
+    url(r'^autocomplete/$', AutoCompleteView.as_view(), name='autocomplete'),
+    url(r'^formtype/$', FormTypeView.as_view(), name='ftype'),
+    url(r'^reindex/$', reindexsolr),
+    url(
+        r'^(?P<case_id>[0-9]*)/(?P<title_slug>.*)/$',
+        CaseReportDetailView.as_view(), name='casereport_detail',
+    ),
+    url(r'^download/(?P<file_id>.*)/$', downloadfile, name='download'),
+    url(
+        r'^edit/(?P<case_id>[0-9]*)/(?P<token>.*)/$',
+        CaseReportEditView.as_view(),
+    ),
+)
