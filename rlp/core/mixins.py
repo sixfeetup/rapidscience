3c27300d97e4ef4877d2264cbdad2a920a96068f
Rob McBroom <rob@sixfeetup.com>
2017-02-10 13:24:34 -0500

    create a mixin for models that share content
    users and groups (projects) in this case

---
+++ rlp/core/mixins.py
@@ -0,0 +1,24 @@
+from django.contrib.contenttypes.fields import GenericRelation
+from django.contrib.contenttypes.models import ContentType
+from django.db import models
+
+from .models import SharedContent
+from rlp.discussions.models import ThreadedComment
+
+
+class SharesContentMixin(models.Model):
+    class Meta:
+        abstract = True
+
+    _shared = GenericRelation(
+        SharedContent,
+        content_type_field='viewer_type',
+        object_id_field='viewer_id',
+    )
+
+    def get_discussions(self):
+        discussion_type = ContentType.objects.get_for_model(ThreadedComment)
+        disc_refs = self._shared.select_related('target_type').filter(
+            target_type=discussion_type
+        )
+        return [dr.target for dr in disc_refs]
