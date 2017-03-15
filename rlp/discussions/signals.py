e2322564e84b84686eb0c62f89a0e3af395bafa9
Rob McBroom <rob@sixfeetup.com>
2017-03-15 10:32:50 -0400

    refactor comment notifications for shared content
    The content_object of a comment usually isn’t the right way to find the object under the new sharing model.
    
    Also, PEP-8 some comments.

--- rlp/discussions/signals.py
+++ rlp/discussions/signals.py
@@ -8,16 +8,17 @@ from django_comments.signals import comment_was_posted
 
 @receiver(comment_was_posted)
 def create_comment_activity(**kwargs):
-    from .models import ThreadedComment
     request = kwargs['request']
-    # Add the message here since the django-contrib-comments view doesn't add a message.
+    # Add the message here since the django-contrib-comments
+    # view doesn't add a message.
     messages.success(request, "Your comment was added!")
     comment = kwargs['comment']
-    # Don't create an action item for duplicate comments. django-contrib-comments will detect duplicates and return the
-    # original comment, so we have to guard against accidentally adding duplicate entries to the activity stream.
+    # Don't create an action item for duplicate comments.
+    # django-contrib-comments will detect duplicates and return the original
+    # comment, so we have to guard against accidentally adding duplicate
+    # entries to the activity stream.
     if comment.action_object_actions.count():
         return
-    viewer = comment.content_object
     if comment.is_reply():
         verb = 'reply'
     else:
@@ -26,4 +27,16 @@ def create_comment_activity(**kwargs):
         comment.user,
         verb=verb,
         action_object=comment,
+    )
+    # find the object being commented on
+    top_comment = comment.discussion_root
+    if comment.is_discussion:
+        content = top_comment
+    else:
+        content = top_comment.content_object
+    content.notify_viewers(
+        '{}: A new comment was posted'.format(
+            settings.SITE_PREFIX.upper(),
+        ),
+        {'action': new_action[0][1]},
     )
