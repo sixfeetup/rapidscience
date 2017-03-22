e557ef692e91b6bda4b828976eca698ee000da21
Glenn Franxman <gfranxman@gmail.com>
2017-03-22 11:46:43 -0400

    If an upload is missing for indexing, note it and keep moving...

--- rlp/documents/search_indexes.py
+++ rlp/documents/search_indexes.py
@@ -10,15 +10,18 @@ class FileIndex(TaggableBaseIndex, indexes.Indexable):
     def get_model(self):
         return File
 
-    def prepare_display_type(self, obj):
-        return obj.display_type
-
     def prepare(self, obj):
         self.prepared_data = super().prepare(obj)
         backend = self._get_backend(None)
-        text = backend.extract_file_contents(obj.upload.path)
-        self.prepared_data['text'] += "\n {}".format(text)
+        if obj.upload.storage.exists( obj.upload.path ):
+            text = backend.extract_file_contents(obj.upload.path)
+            self.prepared_data['text'] += "\n {}".format(text)
+        else:
+            print( "documents.models.File", obj, "is missing its actual file from", obj.upload.path)
         return self.prepared_data
+
+    def prepare_display_type(self, obj):
+        return obj.display_type
 
     def prepare_text(self, obj):
         return 'File'
