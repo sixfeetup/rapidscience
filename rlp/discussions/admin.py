5cdc9cf3088103b3ac208ed5909cd8185fe8f238
Calvin Hendryx-Parker <calvin@sixfeetup.com>
2017-06-01 09:37:14 -0400

    added admin for references and discussions, refs #735, #736

--- rlp/discussions/admin.py
+++ rlp/discussions/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import ThreadedComment
+
+class ThreadedCommentAdmin(admin.ModelAdmin):
+    list_display = ('__str__', 'user_name')
+    list_filter = ('user_name',)
 
 # Register your models here.
+admin.site.register(ThreadedComment, ThreadedCommentAdmin)
