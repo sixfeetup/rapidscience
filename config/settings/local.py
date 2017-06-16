2d54b4718cf34b400777e80a148b35d8d5fb53d6
Glenn Franxman <gfranxman@gmail.com>
2017-06-16 16:26:14 -0400

    Adding django-debug-toolbar-template-profiler to the local config.

--- config/settings/local.py
+++ config/settings/local.py
@@ -26,7 +26,7 @@ EMAIL_BACKEND = env('DJANGO_EMAIL_BACKEND',
 # django-debug-toolbar
 # ------------------------------------------------------------------------------
 MIDDLEWARE_CLASSES += ('debug_toolbar.middleware.DebugToolbarMiddleware',)
-INSTALLED_APPS += ('debug_toolbar', )
+INSTALLED_APPS += ('debug_toolbar', 'template_profiler_panel' )
 
 INTERNAL_IPS = ['127.0.0.1', '10.0.2.2', ]
 # tricks to have debug toolbar when developing with docker
@@ -41,6 +41,23 @@ DEBUG_TOOLBAR_CONFIG = {
     'SHOW_TEMPLATE_CONTEXT': True,
     'JQUERY_URL': '',
 }
+
+
+DEBUG_TOOLBAR_PANELS = [
+    'debug_toolbar.panels.versions.VersionsPanel',
+    'debug_toolbar.panels.timer.TimerPanel',
+    'debug_toolbar.panels.settings.SettingsPanel',
+    'debug_toolbar.panels.headers.HeadersPanel',
+    'debug_toolbar.panels.request.RequestPanel',
+    'debug_toolbar.panels.sql.SQLPanel',
+    'debug_toolbar.panels.staticfiles.StaticFilesPanel',
+    'debug_toolbar.panels.templates.TemplatesPanel',
+    'debug_toolbar.panels.cache.CachePanel',
+    'debug_toolbar.panels.signals.SignalsPanel',
+    'debug_toolbar.panels.logging.LoggingPanel',
+    #'debug_toolbar.panels.redirects.RedirectsPanel',
+    'template_profiler_panel.panels.template.TemplateProfilerPanel',
+]
 
 # django-extensions
 # ------------------------------------------------------------------------------
