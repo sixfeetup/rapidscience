6aca6570a3eca4f60be4c8b4f3093869e1755482
Anthony Bosio <anthony@sixfeetup.com>
2017-06-19 13:51:22 -0400

    disable content truncation. refs #306

--- rlp/core/static/js/main.js
+++ rlp/core/static/js/main.js
@@ -22,39 +22,39 @@ var selectedBookmarksFolderName; // will contain ID of selected bookmarks folder
         }
     });
 
-    var showChar = 170;
-    var ellipsestext = '&hellip;';
-    var moretext = '+';
-    var lesstext = '-';
+    // var showChar = 170;
+    // var ellipsestext = '&hellip;';
+    // var moretext = '+';
+    // var lesstext = '-';
 
     // More or less
-    $('.more').each(function() {
-        var content = $(this).html();
+    // $('.more').each(function() {
+    //     var content = $(this).html();
 
-        if(content.length > showChar) {
+    //     if(content.length > showChar) {
 
-            var c = content.substr(0, showChar);
-            var h = content.substr(showChar, content.length - showChar);
+    //         var c = content.substr(0, showChar);
+    //         var h = content.substr(showChar, content.length - showChar);
 
-            var html = c + '<span class="moreellipses">' + ellipsestext+ '&nbsp;</span><span class="morecontent"><span>' + h + '</span>&nbsp;<a href="" class="morelink">' + moretext + '</a></span>';
+    //         var html = c + '<span class="moreellipses">' + ellipsestext+ '&nbsp;</span><span class="morecontent"><span>' + h + '</span>&nbsp;<a href="" class="morelink">' + moretext + '</a></span>';
 
-            $(this).html(html);
-        }
+    //         $(this).html(html);
+    //     }
 
-    });
+    // });
 
-    $('.container').on('click', '.morelink', function(event){
-        if($(this).hasClass('less')) {
-            $(this).removeClass('less');
-            $(this).html(moretext);
-        } else {
-            $(this).addClass('less');
-            $(this).html(lesstext);
-        }
-        $(this).parent().prev().toggle();
-        $(this).prev().toggle();
-        return false;
-    });
+    // $('.container').on('click', '.morelink', function(event){
+    //     if($(this).hasClass('less')) {
+    //         $(this).removeClass('less');
+    //         $(this).html(moretext);
+    //     } else {
+    //         $(this).addClass('less');
+    //         $(this).html(lesstext);
+    //     }
+    //     $(this).parent().prev().toggle();
+    //     $(this).prev().toggle();
+    //     return false;
+    // });
 
     // hide hiddenField wrappers
     $(".hiddenField").parent(".fieldWrapper").hide();
