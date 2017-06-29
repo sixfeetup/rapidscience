4eda5ece1749da4f67d83c9915e9aacfef94defa
Glenn Franxman <gfranxman@gmail.com>
2017-06-29 15:46:07 -0400

    Index References which have been saved by users, along with their tags.
    refs #279 and #869

--- rlp/bibliography/search_indexes.py
+++ rlp/bibliography/search_indexes.py
@@ -1,13 +1,24 @@
 from haystack import indexes
 
-from .models import Reference
-from rlp.search.search_indexes import BaseIndex
+from .models import Reference, UserReference
+from rlp.search.search_indexes import BaseIndex, TaggableBaseIndex
 
 
-class ReferenceIndex(BaseIndex):
+class ReferenceIndex(TaggableBaseIndex, indexes.Indexable):
+
+    def prepare_tags(self, obj):
+        tags =  [tag.id for tag in obj.tags.all()]
+        urefs = UserReference.objects.filter(reference=obj)
+        for uref in urefs:
+            tags.extend([tag.id for tag in uref.tags.all()])
+        return {*tags}
+
     def prepare_title(self, obj):
         return "{}".format(obj.title)
 
     def get_model(self):
         return Reference
 
+    def index_queryset(self, using=None):
+        user_refs = UserReference.objects.all()
+        return Reference.objects.filter( id__in=[ur.reference_id for ur in user_refs])
