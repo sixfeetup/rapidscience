70c05303b00ec80e984c7df76654e8e65e6c1641
Glenn Franxman <gfranxman@gmail.com>
2017-07-07 10:53:31 -0400

    Adding UserReference to the admin.
    refs #935

--- rlp/bibliography/admin.py
+++ rlp/bibliography/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Reference
+from .models import Reference, UserReference
 
 # Register your models here.
 admin.site.register(Reference)
+admin.site.register(UserReference)
