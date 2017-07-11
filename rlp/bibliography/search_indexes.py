53a48d03a5c74c91a6d961f9ca7129a76bd80568
Glenn Franxman <gfranxman@gmail.com>
2017-07-11 11:51:12 -0400

    More UserReference as replacement for Reference.   Affects Solr
    After this, you'll have to:
        Stop Solr
        env/bin/python manage.py build_solr_schema > ${SOLAR_HOME}/example/solr/collection1/conf/schema.xml
        env/bin/python manage.py build_solr_schema > ${SOLAR_HOME}/example/solr/cases_collection/conf/schema.xml
        Restart Solr
        env/bin/python manage.py rebuild_indexes
    
    refs #792

--- rlp/bibliography/search_indexes.py
+++ rlp/bibliography/search_indexes.py
@@ -6,19 +6,12 @@ from rlp.search.search_indexes import BaseIndex, TaggableBaseIndex
 
 class ReferenceIndex(TaggableBaseIndex, indexes.Indexable):
 
-    def prepare_tags(self, obj):
-        tags =  [tag.id for tag in obj.tags.all()]
-        urefs = UserReference.objects.filter(reference=obj)
-        for uref in urefs:
-            tags.extend([tag.id for tag in uref.tags.all()])
-        return {*tags}
-
     def prepare_title(self, obj):
-        return "{}".format(obj.title)
+        return "{}".format(obj.reference.title)
 
     def get_model(self):
-        return Reference
+        return UserReference
 
     def index_queryset(self, using=None):
         user_refs = UserReference.objects.all()
-        return Reference.objects.filter( id__in=[ur.reference_id for ur in user_refs])
+        return user_refs
