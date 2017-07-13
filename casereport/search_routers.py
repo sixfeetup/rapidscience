15991aec3d79a24b6cf7a81d5a16c0429bf7bc68
Glenn Franxman <gfranxman@gmail.com>
2017-07-13 16:20:08 -0400

    Split CaseReports into Live instances in the default, and all in the CasesCentral.

--- casereport/search_routers.py
+++ casereport/search_routers.py
@@ -2,16 +2,34 @@ import urllib.request
 from django.conf import settings
 from haystack import routers
 
+from casereport.constants import WorkflowState
+from casereport.models import CaseReport
+
 
 class CaseReportRouter(routers.BaseRouter):
     def for_write(self, **hints):
+        obj = hints['instance']
         if check_connection('casescentral'):
-            return 'casescentral'
-        return 'default'
+            if isinstance(obj, CaseReport):
+                return 'casescentral'
+        return
 
     def for_read(self, **hints):
         if check_connection('casescentral'):
             return 'casescentral'
+        return 'default'
+
+class GeneralSearchRouter(routers.DefaultRouter):
+
+    def for_write(self, **hints):
+        """ Only LIVE casereports in the general search.
+        """
+        obj = hints['instance']
+
+        if isinstance(obj, CaseReport):
+            if not obj.workflow_state == WorkflowState.LIVE:
+                return
+
         return 'default'
 
 
