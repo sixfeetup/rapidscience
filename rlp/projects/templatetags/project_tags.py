a88ca380e42713f2c1514c831b42c05415f56a5a
Glenn Franxman <gfranxman@gmail.com>
2017-07-14 11:08:12 -0400

    The show_project_members tag now understands 'ignored' membership requests.

--- rlp/projects/templatetags/project_tags.py
+++ rlp/projects/templatetags/project_tags.py
@@ -20,13 +20,24 @@ def projects_sidebar(context):
     takes_context=True,
 )
 def show_project_members(context, project):
+    request = context['request']
+    user = request.user
+    memberships = project.projectmembership_set.filter(user__is_active=True,
+                                                       state__in=('member',
+                                                                  'moderator'))
+    # members list differs by viewer
+    if user.is_staff or user in project.moderators.all():
+        memberships = project.projectmembership_set.filter(
+            user__is_active=True).exclude(state='ignored')
+
+    memberships = memberships.exclude(state='moderator')
+    memberships = memberships.order_by('state', 'user__first_name')
+
     return {
         'project': project,
         'request': context['request'],
         'moderators': project.projectmembership_set.filter(
             user__is_active=True, state='moderator').order_by(
             'state', 'user__first_name'),
-        'memberships': project.projectmembership_set.filter(
-            user__is_active=True).exclude(state='moderator').order_by(
-            'state', 'user__first_name')
+        'memberships': memberships,
     }
