e4bd76ae78565fe6608a9446346061e0674221e5
Glenn Franxman <gfranxman@gmail.com>
2017-07-14 15:44:31 -0400

    Some bulk-item support for the search_routers related to live/non-live casereports.

--- casereport/search_routers.py
+++ casereport/search_routers.py
@@ -2,15 +2,21 @@ import urllib.request
 from django.conf import settings
 from haystack import routers
 
+import casereport.search_indexes
 from casereport.constants import WorkflowState
 from casereport.models import CaseReport
 
 
 class CaseReportRouter(routers.BaseRouter):
     def for_write(self, **hints):
-        obj = hints['instance']
-        if check_connection('casescentral'):
-            if isinstance(obj, CaseReport):
+        try:
+            obj = hints['instance']
+            if check_connection('casescentral'):
+                if isinstance(obj, CaseReport):
+                    return 'casescentral'
+        except KeyError as no_instance:
+            index = hints['index']
+            if isinstance(index, casereport.search_indexes.CaseReportIndex):
                 return 'casescentral'
         return
 
@@ -24,12 +30,15 @@ class GeneralSearchRouter(routers.DefaultRouter):
     def for_write(self, **hints):
         """ Only LIVE casereports in the general search.
         """
-        obj = hints['instance']
+        try:
+            obj = hints['instance']
 
-        if isinstance(obj, CaseReport):
-            if obj.workflow_state != WorkflowState.LIVE:
-                return
-
+            if isinstance(obj, CaseReport):
+                if obj.workflow_state != WorkflowState.LIVE:
+                    return
+        except KeyError as no_instance:
+            pass
+            # not sure how to exclude non-live casereports here.
         return 'default'
 
 
