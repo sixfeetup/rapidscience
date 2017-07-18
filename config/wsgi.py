d7954e73a7037d9e2ab1fc72877cdf82a3e440ef
Glenn Franxman <gfranxman@gmail.com>
2017-07-18 11:01:35 -0400

    Change stdout and stderr of the wsgi processes to use UTF-8. refs #977
    I set it here as it seemed both the most targeted change and would require the
    fewest esternal changes.

--- config/wsgi.py
+++ config/wsgi.py
@@ -8,9 +8,17 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 
 import os
+import sys
+import io
+
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.production")
+
+# to protect printlogging while still allowing the HTTP headers to drive the
+# response encodings, we'll update stdout and stderr
+sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
+sys.stderr = io.TextIOWrapper(sys.stderr.buffer, encoding='utf-8')
 
 application = get_wsgi_application()
