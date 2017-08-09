51f07566dd97a3abaa75a893d38547a634a190e1
Glenn Franxman <gfranxman@gmail.com>
2017-08-09 10:35:46 -0400

    new rlp.core.utils.resolve_email_targets Given a target comprised of users, projects, strings and return a set         of email addresses in either x@domain.tld or Name <x@domain.tld> format         with duplicates removed and known opt-out's honored.
    refs #999 #1051

--- rlp/core/utils.py
+++ rlp/core/utils.py
@@ -1,4 +1,5 @@
 import sys
+from collections import Iterable, namedtuple
 
 from django.conf import settings
 from django.contrib import messages
@@ -243,3 +244,92 @@ def add_tags(obj, tags):
 
     # Trigger any post-save signals (e.g. Haystack's real-time indexing)
     obj.save()
+
+
+def resolve_email_targets(target):
+    """ Take a target comprised of users, projects, strings and return a set
+        of email addresses in either x@domain.tld or Name <x@domain.tld> format
+        with duplicates removed and known opt-out's honored.
+    """
+    if isinstance(target, str):
+        return set(target)
+    else:
+        # start with a set of items to resolve
+        if isinstance( target, Iterable):
+            starting_set = set(target)
+        else:
+            starting_set = {target}
+
+        # expand any Projects into  Users
+        users_and_strings = set()
+        for item in starting_set:
+            if hasattr(item, 'users'):
+                for m in item.active_members():
+                    users_and_strings.add(m)
+            else:
+                users_and_strings.add(item)
+
+        # email_addresses
+        naas = set()
+        emails = set()
+        NameAndAddress = namedtuple('NameAndAddress', "name address")
+        for recipient in users_and_strings:
+            if hasattr(recipient, "opt_out_of_email"):
+                if recipient.opt_out_of_email:
+                    pass # skip the opted-out target
+                else:
+                    naa = NameAndAddress(recipient.get_full_name(),
+                                         recipient.email)
+                    naas.add(naa)
+            else:
+                naa = NameAndAddress( None, recipient)
+                # that assumes it was a plain email addr
+                naas.add(naa)
+        # now we can result it down to a list of strings
+        for naa in naas:
+            if naa.name:
+                emails.add("{name} <{email}>".format(name=naa.name,
+                                                     email=naa.address))
+            else:
+                emails.add(naa.address)
+        return emails
+
+
+
+def test_resolve_email_targets():
+    from rlp.accounts.models import User
+    from rlp.projects.models import Project
+    from pprint import pprint
+
+    u1 = User.objects.first()
+    u2 = User.objects.last()
+    p1 = Project.objects.first()
+    p2 = Project.objects.last()
+    s1 = "glenn@gmail.com"
+    s2 = "glenn franxman <glenn@sixfeetup.com>"
+    resolve_email_targets(u1)
+    #Out[11]: {'Sixies Up <email>'}
+
+    resolve_email_targets(u2)
+    #Out[12]: {'Glenn OtherUser <email>'}
+
+    resolve_email_targets(p1)
+    #Out[13]: {'Christine Veenstra-VanderSháw <email>',
+    #          'Glenn Superuser <email>'}
+
+    resolve_email_targets(p2)
+    #Out[14]: {'Sarah Greene <email>'}
+
+    resolve_email_targets([u2])
+    resolve_email_targets([p2])
+    resolve_email_targets([s2])
+
+    pprint(resolve_email_targets((u1, u2, p1, p2, s1, s2)))
+    pprint(resolve_email_targets((u1, u1, p1, p1, s1, s1)))
+    print(u1, "opting out")
+    u1.opt_out_of_email = True
+    res = resolve_email_targets((u1, u1, p1, p1, s1, s1))
+    pprint(res)
+    assert u1 not in res, "opt-out failed"
+    assert s1 in res
+
