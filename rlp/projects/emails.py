ab88e3355d0814a8d05167231fc3fc96b281bc15
osiris <osirisgclark@gmail.com>
2017-09-15 15:40:43 -0400

    Change from line on approve to join closed groups. ref #928
    change from settings.DEFAULT_FROM_EMAIL to "Sarcoma Central with the email address support@rapidscience.org".
    and change the signature to be "The <Sarcoma Central> team <@RapidScience>

--- rlp/projects/emails.py
+++ rlp/projects/emails.py
@@ -45,7 +45,7 @@ def approve_to_requester(request, membership, group):
     template = "projects/emails/approve_to_requester"
     body = render_to_string('{}.txt'.format(template), data)
     mail = EmailMessage(subject, body,
-                        settings.DEFAULT_FROM_EMAIL,
+                        "Sarcoma Central with the email address support@rapidscience.org",
                         [membership.user.email, ])
     mail.content_subtype = "html"
     mail.send()
