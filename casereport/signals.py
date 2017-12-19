a8f670a20de2a246eb66e85f70a7745c03c0b3a4
Rob McBroom <rob@sixfeetup.com>
2017-12-19 10:01:54 -0500

    reindex case reports when author profile changes
    refs #1248

--- casereport/signals.py
+++ casereport/signals.py
@@ -1,8 +1,12 @@
+from django.db.models import Q
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 from .emails import created
 from .models import CaseReport
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 
 @receiver(post_save, sender=CaseReport)
@@ -11,3 +15,17 @@ def casereport_saved(sender, **kwargs):
         return
     report = kwargs.get('instance')
     created(report)
+
+
+@receiver(post_save, sender=User)
+def author_updated(sender, **kwargs):
+    '''reindex an author's case reports when their profile is updated'''
+    if kwargs.get('update_fields') == {'last_login'}:
+        # ignore update during login
+        return
+    user = kwargs.get('instance')
+    authored = Q(primary_author=user)
+    co_authored = Q(co_author=user)
+    user_reports = CaseReport.objects.filter(authored | co_authored)
+    for cr in user_reports:
+        post_save.send(sender=CaseReport, instance=cr)
