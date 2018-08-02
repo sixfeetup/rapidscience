734dd10740e0dd7e2987de51ce084a62d2230525
Anthony Bosio <anthony@sixfeetup.com>
2018-08-02 14:10:50 -0400

    fix highlight js so it can be pre-compiled. refs #1423
    - can't use request data in pre-compiled js
    - use js to get the keyword(s) from the url instead

--- rlp/core/static/js/main.js
+++ rlp/core/static/js/main.js
@@ -399,3 +399,12 @@ $(".sub-menu").on('submit', function(e) {
   });
 
 }); })(jQuery);
+
+function getQueryParam(param) {
+    location.search.substr(1)
+        .split("&")
+        .some(function(item) { // returns first occurrence and stops
+            return item.split("=")[0] == param && (param = item.split("=")[1])
+        });
+    return param
+}
